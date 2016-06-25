7e51908ac21278801c3a220b03492a1d027d7036
ricardobarros95 <ricardobarros95@gmail.com>
2016-06-26 00:37:39 +0100

    commits

--- Blobby/Assets/Script/GameManager.cs
+++ Blobby/Assets/Script/GameManager.cs
@@ -10,7 +10,11 @@ public class GameManager : MonoBehaviour {
 
 
     public static Color orange = new Color(1, 0.647059f, 0, 1);
-    public static Color purple = new Color(0.627451f, 0.12549f, 0.941176f, 1);  
+    public static Color purple = new Color(0.627451f, 0.12549f, 0.941176f, 1);
+
+    public Sprite[] colorSprites;
+    public GameObject goalSprite1;
+    public GameObject goalSprite2;
 
     void Start()
     {
@@ -25,17 +29,22 @@ public class GameManager : MonoBehaviour {
             goals = Random.Range(0, 2);
             if(goals == 0)
             {
-                goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.red;
-                goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.green;
+                //goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.red;
+                //goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.green;
                 goalObjects[0].GetComponent<GoalColor>().color = Colors.RED;
                 goalObjects[1].GetComponent<GoalColor>().color = Colors.GREEN;
+                goalSprite1.GetComponent<SpriteRenderer>().sprite = colorSprites[1];
+                goalSprite2.GetComponent<SpriteRenderer>().sprite = colorSprites[0];
+
             }
             else
             {
-                goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.green;
-                goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.red;
+                //goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.green;
+                //goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.red;
                 goalObjects[0].GetComponent<GoalColor>().color = Colors.GREEN;
                 goalObjects[1].GetComponent<GoalColor>().color = Colors.RED;
+                goalSprite1.GetComponent<SpriteRenderer>().sprite = colorSprites[0];
+                goalSprite2.GetComponent<SpriteRenderer>().sprite = colorSprites[1];
             }
         }
         else if( goals == 1)
@@ -43,17 +52,21 @@ public class GameManager : MonoBehaviour {
             goals = Random.Range(0, 1);
             if(goals == 0)
             {
-                goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.blue;
-                goalObjects[1].GetComponent<MeshRenderer>().material.color = orange;
+                //goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.blue;
+                //goalObjects[1].GetComponent<MeshRenderer>().material.color = orange;
                 goalObjects[0].GetComponent<GoalColor>().color = Colors.BLUE;
                 goalObjects[1].GetComponent<GoalColor>().color = Colors.ORANGE;
+                goalSprite1.GetComponent<SpriteRenderer>().sprite = colorSprites[2];
+                goalSprite2.GetComponent<SpriteRenderer>().sprite = colorSprites[3];
             }
             else
             {
-                goalObjects[0].GetComponent<MeshRenderer>().material.color = orange;
-                goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.blue;
+                //goalObjects[0].GetComponent<MeshRenderer>().material.color = orange;
+                //goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.blue;
                 goalObjects[0].GetComponent<GoalColor>().color = Colors.ORANGE;
                 goalObjects[1].GetComponent<GoalColor>().color = Colors.BLUE;
+                goalSprite1.GetComponent<SpriteRenderer>().sprite = colorSprites[3];
+                goalSprite2.GetComponent<SpriteRenderer>().sprite = colorSprites[2];
             }
         }
         else
@@ -61,17 +74,21 @@ public class GameManager : MonoBehaviour {
             goals = Random.Range(0, 1);
             if(goals == 0)
             {
-                goalObjects[0].GetComponent<MeshRenderer>().material.color = purple;
-                goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.yellow;
+                //goalObjects[0].GetComponent<MeshRenderer>().material.color = purple;
+                //goalObjects[1].GetComponent<MeshRenderer>().material.color = Color.yellow;
                 goalObjects[0].GetComponent<GoalColor>().color = Colors.PURPLE;
                 goalObjects[1].GetComponent<GoalColor>().color = Colors.YELLOW;
+                goalSprite1.GetComponent<SpriteRenderer>().sprite = colorSprites[4];
+                goalSprite2.GetComponent<SpriteRenderer>().sprite = colorSprites[5];
             }
             else
             {
-                goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.yellow;
-                goalObjects[1].GetComponent<MeshRenderer>().material.color = purple;
+                //goalObjects[0].GetComponent<MeshRenderer>().material.color = Color.yellow;
+                //goalObjects[1].GetComponent<MeshRenderer>().material.color = purple;
                 goalObjects[0].GetComponent<GoalColor>().color = Colors.YELLOW;
                 goalObjects[1].GetComponent<GoalColor>().color = Colors.PURPLE;
+                goalSprite1.GetComponent<SpriteRenderer>().sprite = colorSprites[5];
+                goalSprite2.GetComponent<SpriteRenderer>().sprite = colorSprites[4];
             }
         }
     }
